2418b9954d0f0f2d0bee834c916cfe6ceecf9f5c
Charles Lombardo <clombardo169@gmail.com>
2025-02-12 20:28:05 -0500

    Rework notification defaults
    Timer, stopwatch, upcoming alarm, and snoozed alarm notifications will all now appear with default importance and use no sound nor vibration. This ensures that they're all accessible from the lock screen.
    This also fixes behavior where firing notifications would not appear as a pop-up and makes missed alarm notifications high priority.

--- app/src/main/kotlin/com/android/deskclock/NotificationUtils.kt
+++ app/src/main/kotlin/com/android/deskclock/NotificationUtils.kt
@@ -21,9 +21,10 @@ import android.app.NotificationChannel
 import android.content.Context
 import android.util.ArraySet
 import android.util.Log
+import androidx.annotation.StringRes
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.NotificationManagerCompat.IMPORTANCE_DEFAULT
 import androidx.core.app.NotificationManagerCompat.IMPORTANCE_HIGH
-import androidx.core.app.NotificationManagerCompat.IMPORTANCE_LOW
 
 object NotificationUtils {
     private val TAG = NotificationUtils::class.java.simpleName
@@ -58,40 +59,39 @@ object NotificationUtils {
      */
     const val STOPWATCH_NOTIFICATION_CHANNEL_ID = "stopwatchNotification"
 
-    /**
-     * Values used to bitmask certain channel defaults
-     */
-    private const val PLAY_SOUND = 0x01
-    private const val ENABLE_LIGHTS = 0x02
-    private const val ENABLE_VIBRATION = 0x04
+    private data class ChannelProperties(
+        @StringRes val nameId: Int,
+        val importance: Int = IMPORTANCE_DEFAULT,
+        val vibration: Boolean = false,
+        val sound: Boolean = false,
+        val lights: Boolean = false
+    )
 
-    private val CHANNEL_PROPS: MutableMap<String, IntArray> = HashMap()
+    private val CHANNEL_PROPS: MutableMap<String, ChannelProperties> = HashMap()
 
     init {
-        CHANNEL_PROPS[ALARM_MISSED_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.alarm_missed_channel,
-                IMPORTANCE_HIGH
+        CHANNEL_PROPS[ALARM_MISSED_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.alarm_missed_channel,
+            importance = IMPORTANCE_HIGH,
+            vibration = true,
+            sound = true
         )
-        CHANNEL_PROPS[ALARM_SNOOZE_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.alarm_snooze_channel,
-                IMPORTANCE_LOW
+        CHANNEL_PROPS[ALARM_SNOOZE_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.alarm_snooze_channel
         )
-        CHANNEL_PROPS[ALARM_UPCOMING_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.alarm_upcoming_channel,
-                IMPORTANCE_LOW
+        CHANNEL_PROPS[ALARM_UPCOMING_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.alarm_upcoming_channel
         )
-        CHANNEL_PROPS[FIRING_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.firing_alarms_timers_channel,
-                IMPORTANCE_HIGH,
-                ENABLE_LIGHTS
+        CHANNEL_PROPS[FIRING_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.firing_alarms_timers_channel,
+            importance = IMPORTANCE_HIGH,
+            lights = true
         )
-        CHANNEL_PROPS[STOPWATCH_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.stopwatch_channel,
-                IMPORTANCE_LOW
+        CHANNEL_PROPS[STOPWATCH_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.stopwatch_channel
         )
-        CHANNEL_PROPS[TIMER_MODEL_NOTIFICATION_CHANNEL_ID] = intArrayOf(
-                R.string.timer_channel,
-                IMPORTANCE_LOW
+        CHANNEL_PROPS[TIMER_MODEL_NOTIFICATION_CHANNEL_ID] = ChannelProperties(
+            nameId = R.string.timer_channel
         )
     }
 
@@ -108,15 +108,11 @@ object NotificationUtils {
         }
 
         val properties = CHANNEL_PROPS[id]!!
-        val nameId = properties[0]
-        val channel = NotificationChannel(id, context.getString(nameId), properties[1])
-        if (properties.size >= 3) {
-            val bits = properties[2]
-            channel.enableLights(bits and ENABLE_LIGHTS != 0)
-            channel.enableVibration(bits and ENABLE_VIBRATION != 0)
-            if (bits and PLAY_SOUND == 0) {
-                channel.setSound(null, null)
-            }
+        val nameId = properties.nameId
+        val channel = NotificationChannel(id, context.getString(nameId), properties.importance)
+        channel.enableVibration(properties.vibration)
+        if (!properties.sound) {
+            channel.setSound(null, null)
         }
         val nm: NotificationManagerCompat = NotificationManagerCompat.from(context)
         nm.createNotificationChannel(channel)
